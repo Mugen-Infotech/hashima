444dd4268aed56b63e219291c4def5556a4cd0f7
Iram Shahzad <iramshahzad@yahoo.com>
2025-06-29 07:59:53 +0600

    added contact link

--- wp-content/themes/hashima/TrailerTemplate.php
+++ wp-content/themes/hashima/TrailerTemplate.php
@@ -411,10 +411,12 @@ get_header();
               <div
                 class="absolute top-1 left-1 right-1 md:right-[3px] xl:right-1 bottom-1 border border-black rounded-full"></div>
             </div>
-            HASHIMA BASEでの撮影をご希望の方へ<br />
-            HASHIMA BASEでは、撮影場所としてスペースの貸し出しを行っております。<br />
-            施設全体のご利用が可能で、貸し出し時間は【1時間から】承っております。<br />
-            ご希望の方は、お気軽に[CONTACT]ページよりお問い合わせください。
+            <span>
+              HASHIMA BASEでの撮影をご希望の方へ<br />
+              HASHIMA BASEでは、撮影場所としてスペースの貸し出しを行っております。<br />
+              施設全体のご利用が可能で、貸し出し時間は【1時間から】承っております。<br />
+              ご希望の方は、お気軽に[<a href="<?php echo home_url('contact') ?>" class="underline text-blue-600 hover:text-blue-800">CONTACT</a>]ページよりお問い合わせください。
+            </span>
           </li>
         </ul>
       </div>
